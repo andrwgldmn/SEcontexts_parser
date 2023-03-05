7e857df6c4003271d427fdf612a3a28690689b8c
Andrew <poisk567@gmail.com>
2023-03-05 18:50:55 +0200

    English lang only

--- README.md
+++ README.md
@@ -1,33 +1,5 @@
 ﻿# SEcontexts_parser
 
-Парсер контекстов сеполиси (property_/service_/file_)
-
-Парсер стоковых бинарников file_contexts/sepolicy
-
-Парсер логкатов для получения правил сеполиси локально и через Интернет
-
-### Как использовать?
-
-```
-sudo apt-get install python-dev python3-dev python-requests
-
-python SEcontexts_parser.py
-```
-
-### Использование на Windows
-
-1. Скачать Python 3 с официального сайта
-
-2. Запустить скрипт SEcontexts_parser-win.py
-
-Парсинг logcat через Интернет не работает на Windows из-за отсутствия библиотеки requests
-
-Спасибо https://ru.stackoverflow.com/users/213678/andrey и https://ru.stackoverflow.com/users/201445/gil9red и https://github.com/TheDeadOne и https://ru.stackoverflow.com/users/211923/maxu
-
-## ​----------​----------​----------​----------​----------​------------------​----------​----------​
-
-# SEcontexts_parser
-
 Parsing sepolicy contexts (property_/service_/file_)
 
 Parsing stock binaries file_contexts/sepolicy
@@ -41,14 +13,6 @@ sudo apt-get install python-dev python3-dev python-requests
 
 python SEcontexts_parser.py
 ```
-
-### Using on Windows
-
-1. Download Python 3 from official site
-
-2. Run script SEcontexts_parser-win.py
-
-Logcat parsing via Internet is not working on Windows because requests library is not included in Windows default installation
 
 Thanks to https://ru.stackoverflow.com/users/213678/andrey and https://ru.stackoverflow.com/users/201445/gil9red and https://github.com/TheDeadOne and https://ru.stackoverflow.com/users/211923/maxu
 
--- SEcontexts_parser.py
+++ SEcontexts_parser.py
@@ -9,8 +9,6 @@ value = None
 def help():
         print (' ----------------------------')
         print ('\n Eng:\n Put near this script your file_contexts.txt/property_contexts.txt/service_contexts.txt to work with 1-5 items, your sepolicy and file_contexts binary to work with 6-7  items, your log.txt or a dmesg file which can be renamed to it (its doesnt matter) to work with 8 item, copy a link of your logcat from Web to work with 9 item. \n')
-        print (' \n Рус: Положите рядом со скриптом ваши file_contexts.txt/property_contexts.txt/service_contexts.txt для работы с 1-5 пунктами, ваши бинарники sepolicy и file_contexts    для работы с 6-7 пунктами, ваш log.txt или это может быть файл dmesg, который просто переименован в log.txt (не имеет значения) для работы с 8 пунктом, скопируйте      ссылку на ваш логкат из сети для работы с 9 пунктом. \n ')
-        print (' \n Укр: Покладіть біля цього скрипта ваші file_contexts.txt/property_contexts.txt/service_contexts.txt для роботи з 1-5 пунктами, ваші бінарі sepolicy і file_contexts для роботи з 6-7 пунктами, ваш log.txt або це може бути файл dmesg, який просто перейменований в log.txt (не має значення) для роботи з 8 пунктом, скопіюйте ссилку на ваш  логкат з мережі для роботи з 9 пунктом. \n ')
         print (' \n Made by andrwgldmn \n') 
         print (' ----------------------------')
 
@@ -182,7 +180,7 @@ def cleanup():
 while value != 0:
     help()
     print (' ----------------------------')
-    value = int(input(" 1) English \n 2) Русский \n 3) Українська \n ---------------------------- \n "))
+    value = int(input(" 1) English \n ---------------------------- \n "))
     if (value != 1 and value != 2 and value != 3 ):
         print ("\n Program has been terminated. \n" )
     if (value == 1):
@@ -192,90 +190,6 @@ while value != 0:
             
             if (value == 0):
                 print('-' * 28 + '\n Thanks!\n' + '-' * 28)
-
-            elif (value == 1):
-                property_contexts()
-
-            elif (value == 3):
-                parse_fcf()
-                parse_fcd()
-                parse_fce()
-
-            elif (value == 2):
-                service_contexts()
-
-            elif (value == 4):
-                parse_fcf()
-                parse_fcd()
-                parse_fce_domains()       
-
-            elif (value ==5):
-                only_domains()
-
-            elif (value == 6):
-                stock_fc()
-
-            elif (value == 7):
-                stock_sepo()
-
-            elif (value == 8):
-                sepologparser_local()
-
-            elif (value == 9):
-                sepologparser_inet()
-
-            elif (value == 00):
-                cleanup()
-
-    if (value == 2):
-        while value != 0:
-            print (' ----------------------------')
-            value = int(input(" Выберите категорию: \n \n 0) Выход \n 00) Очистка \n 1) Анализ property_contexts \n 2) Анализ service_contexts \n 3) Анализ file_contexts \n 4) Анализ file_contexts из созданием типов \n 5) Создание ТОЛЬКО типов \n 6) Анализ стокового бинарника file_contexts (взятого из boot.img) для получения стоковых политик \n 7) Анализ стокового бинарника sepolicy (взятого из boot.img) для получения стоковых политик \n 8) Анализ локального log.txt для получения правил SEPolicy \n 9) Анализ log.txt для получения правил SEPolicy через Internet (log.txt будет скачан по ссылке, которую Вы введёте. URL) \n   ---------------------------- \n "))
-            
-            if (value == 0):
-                print('-' * 28 + '\n Спасибо!\n' + '-' * 28)
-
-            elif (value == 1):
-                property_contexts()
-
-            elif (value == 3):
-                parse_fcf()
-                parse_fcd()
-                parse_fce()
-
-            elif (value == 2):
-                service_contexts()
-
-            elif (value == 4):
-                parse_fcf()
-                parse_fcd()
-                parse_fce_domains()       
-
-            elif (value ==5):
-                only_domains()
-
-            elif (value == 6):
-                stock_fc()
-
-            elif (value == 7):
-                stock_sepo()
-
-            elif (value == 8):
-                sepologparser_local()
-
-            elif (value == 9):
-                sepologparser_inet()
-
-            elif (value == 00):
-                cleanup()
-
-    if (value == 3):
-        while value != 0:
-            print (' ----------------------------')
-            value = int(input(" Оберіть категорію: \n \n 0) Вихід \n 00) Очистка \n 1) Аналіз property_contexts \n 2) Аналіз service_contexts \n 3) Аналіз file_contexts \n 4) Аналіз file_contexts зі створенням політик  \n 5) Створення ТІЛЬКИ політик \n 6) Аналіз стокового бінарника file_contexts (взятий з boot.img) для отримання стокових політик \n 7) Аналіз стокового бінарника sepolicy (взятий з boot.img) для отримання стокових політик \n 8) Аналіз локального log.txt для отримання правил SEPolicy \n 9) Аналіз log.txt для отримання правил SEPolicy через Internet (log.txt буде завантажений з URL, яку Ви введете) \n  ---------------------------- \n "))
-            
-            if (value == 0):
-                print('-' * 28 + '\n Дякую!\n' + '-' * 28)
 
             elif (value == 1):
                 property_contexts()
